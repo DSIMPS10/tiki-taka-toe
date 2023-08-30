b4f0796d0b1494c1de6c3e630d555464e2150382
Tom Diamond <84464917+tomdiamond11@users.noreply.github.com>
2023-08-30 14:20:12 +0100

    updates

---
+++ src/scoring.py
@@ -0,0 +1,27 @@
+#TODO: Get all available players which have played for both teams once a grid position has been picked
+def all_correct_answers(team_a, team_b) -> list:
+    all_possible_players = []
+    # Add code here
+
+    return all_possible_players
+
+#TODO: Get total number of guesses for each players
+def total_number_of_guess(all_possible_players) -> dict:
+    # Get guesses list of players from DB guesses table
+
+
+    # Ouput from GET request
+    player_guess_dict = {'player_a': 1,
+                        'player_b': 2,
+                        'player_c': 3}
+    
+    return player_guess_dict
+
+#TODO: Get the relative score of chosen player
+def score_for_selected_player(player, player_guess_dict):
+    player_score = 0
+    # Code to get the percentage score of each player based on number of guesses
+
+    return player_score
+
+    
