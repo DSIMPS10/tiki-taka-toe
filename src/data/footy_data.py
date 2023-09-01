47734cbd0971532056911bb34633b3ee1f08249a
Tom Diamond <thomasodiamond@gmail.com>
2023-09-01 09:52:21 +0100

    update

--- src/data/footy_data.py
+++ src/data/footy_data.py
@@ -13,6 +13,11 @@ headers = {
 
 # conn.request("GET", "/players/squads?team=33", headers=headers)
 
+def get_list_of_seasons(start_date=1993, end_date=2023):
+    season_list = [x for x in range(start_date, end_date+1)]
+    return season_list
+        
+
 def response_to_json(request):
     res = conn.getresponse()
     data = res.read()
@@ -39,17 +44,39 @@ def player_data(player_id):
     return player_team
 
 def player_for_a_season(player_id, season):
-    
     conn.request("GET", f"/players?id={player_id}&season={season}", headers=headers)
     res = conn.getresponse()
     data = res.read()
     decoded = data.decode("utf-8")
     player_json = json.loads(decoded)
     player_response = player_json['response']
-    player_info = player_response[0]
-    player_stats = player_info['statistics']
-    player_team = player_stats[0]['team']
+    try: 
+        player_info = player_response[0]
+        player_stats = player_info['statistics']
+        player_team = player_stats[0]['team']['name']
+    except IndexError as e:
+        player_team = "No value"
     print(player_team)
+    
+def get_all_teams_of_player(player_id: int, seasons:list) -> dict:
+    player_teams: dict = {}
+    for season in seasons:
+        conn.request("GET", f"/players?id={player_id}&season={season}", headers=headers)
+        res = conn.getresponse()
+        data = res.read()
+        decoded = data.decode("utf-8")
+        player_json = json.loads(decoded)
+        player_response = player_json['response']
+        try: 
+            player_info = player_response[0]
+            player_stats = player_info['statistics']
+            player_team = player_stats[0]['team']['name']
+            player_teams[season] = player_team
+        except IndexError as e:
+            continue
+        
+    return player_teams
+    
 
 def main():
     #harry mag
@@ -58,10 +85,13 @@ def main():
     #wan bas
     player_id = 18846
     
-    season = 2016
     # player_data(player_id)
-    player_for_a_season(player_id, season)
+    # player_for_a_season(player_id, season)
     # print(player_for_a_season)
-
+    season_list = get_list_of_seasons(2016, 2022)
+    print(season_list)
+    player_teams = get_all_teams_of_player(player_id, season_list)
+    print(player_teams)
+    
 if __name__ == "__main__":
     main()
