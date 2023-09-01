5d8e6b91acb3abc84372a98a35f174e11395c257
Tom Diamond <thomasodiamond@gmail.com>
2023-09-01 09:36:14 +0100

    minor

--- src/data/footy_data.py
+++ src/data/footy_data.py
@@ -6,18 +6,21 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("FOOTBALL_API_KEY")
 conn = http.client.HTTPSConnection("v3.football.api-sports.io")
-
-
+headers = {
+    'x-rapidapi-host': "v3.football.api-sports.io",
+    'x-rapidapi-key': api_key
+    }
 
 # conn.request("GET", "/players/squads?team=33", headers=headers)
 
+def response_to_json(request):
+    res = conn.getresponse()
+    data = res.read()
+    decoded = data.decode("utf-8")
+    data_json = json.loads(decoded)
+
 def player_data(player_id):
    
-    headers = {
-        'x-rapidapi-host': "v3.football.api-sports.io",
-        'x-rapidapi-key': api_key
-        }
-    
     conn.request("GET", f"/players/squads?player={player_id}", headers=headers)
 
     res = conn.getresponse()
@@ -35,13 +38,30 @@ def player_data(player_id):
     
     return player_team
 
+def player_for_a_season(player_id, season):
+    
+    conn.request("GET", f"/players?id={player_id}&season={season}", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    decoded = data.decode("utf-8")
+    player_json = json.loads(decoded)
+    player_response = player_json['response']
+    player_info = player_response[0]
+    player_stats = player_info['statistics']
+    player_team = player_stats[0]['team']
+    print(player_team)
+
 def main():
     #harry mag
     # player_id = 2935
 
     #wan bas
     player_id = 18846
-    player_data(player_id)
+    
+    season = 2016
+    # player_data(player_id)
+    player_for_a_season(player_id, season)
+    # print(player_for_a_season)
 
 if __name__ == "__main__":
     main()
