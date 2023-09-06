82324b91c6ff7124575f710c61e54a12180ca231
DSIMPS10 <126457997+DSIMPS10@users.noreply.github.com>
2023-09-06 11:11:52 +0100

    j

--- src/flask_pkg/project/routes.py
+++ src/flask_pkg/project/routes.py
@@ -102,16 +102,21 @@ def post_players():
 
 @main.put("/api/update_player_first_season/<string:identifier>/<int:first_season>")
 def update_player_season(identifier, first_season):
+    
     player_name = (" ").join(identifier.split("-")[:-1])
     player_team = identifier.split("-")[-1]
     updated_season_row = Players.query.filter_by(full_name=player_name, team_name=player_team).first()
     updated_season_row.first_season = first_season
     db.session.commit()
-    return print(f'{identifier} first season has been updated to {first_season}')
+    player_info_dict = updated_season_row.as_dict() 
+
+    return jsonify(player_info_dict)
+
+    #return print(f'{identifier} first season has been updated to {first_season}')
 
     # player_jsons = request.get_json()
     # player_dicts = json.loads(player_jsons)  
     # players_to_add = [Players(**row) for row in player_dicts]
     # db.session.add_all(players_to_add)
     # db.session.commit()
-    # return player_jsons
+     
