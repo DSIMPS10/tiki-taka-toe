3a04f6361845e8631ac2e5e77bc122019da4bacd
Tom Diamond <thomasodiamond@gmail.com>
2023-09-13 16:53:56 +0100

    test

--- src/flask_pkg/project/routes.py
+++ src/flask_pkg/project/routes.py
@@ -90,8 +90,9 @@ def get_all_players():
 def get_all_valid_player_guesses(): 
     # all_valid_players = Players.query(func.count(Players.full_name)).group_by(Players.full_name).filter(func.count(Players.full_name)>1).all()
     #player_count = db.func.count(Players.full_name).group_by(Players.full_name).having(player_count > 0).label("player_count")
-    sql = text('SELECT COUNT(full_name),full_name FROM Players GROUP BY full_name HAVING COUNT(full_name)>1')
-    all_valid_players = Players.query.from_statement(sql).all()
+    # sql = text('SELECT COUNT(full_name),full_name FROM Players GROUP BY full_name HAVING COUNT(full_name)>1')
+    # all_valid_players = session.query(Players).from_statement(sql).all()
+    all_valid_players = db.session.execute('SELECT COUNT(full_name),full_name FROM Players GROUP BY full_name HAVING COUNT(full_name)>1')
     print(all_valid_players)
     all_valid_players_array = [player.full_name for player in all_valid_players]
     return jsonify(all_valid_players_array)
